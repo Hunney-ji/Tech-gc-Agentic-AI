629b0c442ee55672736da0407890e22483aa453c
aman2603tiwari <aman_t@ece.iitr.ac.in>
2026-04-09 15:35:43 +0530

    aaloo

--- trycal/try.py
+++ trycal/try.py
@@ -78,16 +78,3 @@ def llm_call(state: dict):
         ],
         "llm_calls": state.get('llm_calls', 0) + 1
     }
-
-from langchain.messages import ToolMessage
-
-
-def tool_node(state: dict):
-    """Performs the tool call"""
-
-    result = []
-    for tool_call in state["messages"][-1].tool_calls:
-        tool = tools_by_name[tool_call["name"]]
-        observation = tool.invoke(tool_call["args"])
-        result.append(ToolMessage(content=observation, tool_call_id=tool_call["id"]))
-    return {"messages": result}
